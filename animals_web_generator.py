3e1e83171c1da85b8bdbf455733e9ecd41e0b7d8
EAtmaca27 <enesatmaca27@googlemail.com>
2026-03-26 17:20:56 +0100

    bring animal data to new html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -12,32 +12,34 @@ def get_animals_data():
     return animals_data
 
 
-def print_animals_data(animals_data):
-    """
-    Takes a list of animal dictionaries and prints
-    details about each animal, such as their name, diet, type
-    and the first location if information is provided.
-
-    :param animals_data: A list of dictionaries, each representing
-    an animal and its associated data.
-    :return: None
-    """
+def generate_animals_data(animals_data):
+    output = ""
     for animal in animals_data:
         if "name" in animal:
-            print(f"Name: {animal['name']}")
+            output += f"Name: {animal['name']}\n"
 
         # Check if characteristics exists, then check if diet and type exist
         if "characteristics" in animal:
             characteristic = animal['characteristics']
 
             if "diet" in characteristic:
-                print(f"Diet: {characteristic['diet']}")
+                output += f"Diet: {characteristic['diet']}\n"
 
             if "type" in characteristic:
-                print(f"Type: {characteristic['type']}")
+                output += f"Type: {characteristic['type']}\n"
 
         # Check if location exists and not empty
         if "locations" in animal and len(animal['locations']) > 0:
-            print(f"Location: {animal['locations'][0]}")
+            output += f"Location: {animal['locations'][0]}\n"
 
-        print()
+    return output
+
+animals_info = generate_animals_data(get_animals_data())
+
+with open("animals_template.html", "r") as file:
+    current_html = file.read()
+
+new_html = current_html.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+
+with open("animals.html", "w") as file:
+    file.write(new_html)
